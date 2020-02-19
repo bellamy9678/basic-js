bfd8dc7328e4d3840014dbaa38586f9541a9843a
Michael Fursa <bellamy9678@yahoo.com>
2020-02-19 23:37:27 +0300

    feat: add dreamteam

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,19 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  
+  if (!Array.isArray(members)) {
+    return false;
+  }
+
+  let name = '';
+  let letter = '';
+
+  for (let i = 0; i < members.length; i++){
+    if (typeof(members[i]) == 'string' && isNaN(members[i]) ) {
+      letter = members[i].split(' ').join('');
+      name+=letter[0];
+    }
+  }
+
+  return name.toLocaleUpperCase().split('').sort().join('');
+
 };
