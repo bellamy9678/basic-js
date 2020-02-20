da21a8126f1a3eaaf3c1b08457dc105ecbce8768
Michael Fursa <bellamy9678@yahoo.com>
2020-02-20 11:45:31 +0300

    feat: hanoi

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,4 +1,9 @@
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+    let turnsAmount = (Math.pow(2, disksNumber) - 1);
+    let speedInSeconds = (turnsSpeed / 3600 );
+    
+    return {
+        turns : turnsAmount,
+        seconds : +((turnsAmount / speedInSeconds).toPrecision(17))
+    };
 }
