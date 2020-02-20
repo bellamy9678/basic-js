90b3ae5f040fd8849255f7bc3095039fb7f709a8
Michael Fursa <bellamy9678@yahoo.com>
2020-02-20 20:36:58 +0300

    feat: simple chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,23 +1,37 @@
 const chainMaker = {
+  out : [],
+
   getLength() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    return this.out.length;
   },
+
   addLink(value) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    if (arguments.length != 0) {
+      this.out.push('( ' + value + ' )');
+    } else {
+      this.out.push('( )');
+    }
+    return this;
   },
+
   removeLink(position) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    if (!Number.isInteger(position) || position <= 0 || position > this.getLength() ) {
+      this.out = [];
+      throw new Error('not valid position');
+    }
+    this.out.splice(position - 1, 1);
+    return this;
   },
+
   reverseChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.out = this.out.reverse();
+    return this;
   },
+
   finishChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    let str = this.out.join('~~');
+    this.out = [];
+    return str;
   }
 };
 
