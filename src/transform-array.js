d68e8ce9fc082a5db479dff51e7d626e92203f9a
Michael Fursa <bellamy9678@yahoo.com>
2020-02-20 15:53:10 +0300

    feat: transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,58 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    
+    if ( !Array.isArray(arr) ) {
+        throw new Error('It is not an Array');
+    }
+
+    let output = [];
+    const   DIS_NEXT = "--discard-next",
+            DIS_PREV = "--discard-prev",
+            DUB_NEXT = "--double-next",
+            DUB_PREV = "--double-prev";
+
+    for (let i = 0; i < arr.length; i++) {
+
+        switch(arr[i]) {
+
+            case DIS_NEXT: {
+                i++;
+                break;
+            }
+
+            case DIS_PREV: {
+                if (arr[i-1] != undefined) {
+                    output.pop();
+                    break;
+                } else {
+                    break;
+                }
+            }
+
+            case DUB_NEXT: {
+                if (arr[i+1] != undefined) {
+                    output.push(arr[i+1]);
+                    break;
+                } else {
+                    break;
+                }
+            }
+
+            case DUB_PREV: {
+                if (arr[i-1] != undefined) {
+                    output.push(arr[i-1]);
+                    break;
+                } else {
+                    break;
+                }
+            }
+            
+            default: {
+                output.push(arr[i]);
+            }
+
+        }
+    }
+
+    return output;
+    
 };
