55943958627432c0bb6019bb1463bf9703aad685
Michael Fursa <bellamy9678@yahoo.com>
2020-03-05 14:49:57 +0300

    feat: add recursive task

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,6 +1,17 @@
 module.exports = class DepthCalculator {
-    calculateDepth(/* arr */) {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    calculateDepth(arr) {
+        if ( Array.isArray(arr) ) {
+            let maxdep = 1;
+            for (let i = 0; i < arr.length; i++) {
+                let dep = 1;
+                dep += this.calculateDepth(arr[i]);
+                if (dep > maxdep) {
+                    maxdep = dep;
+                }
+            }
+            return maxdep;
+        } else {
+            return 0;
+        }
     }
 };
